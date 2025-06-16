4675c8ed5ab255c136b20f3a48977d0194013dde
Eduoliveira1907 <eduardo.l.oliveira19@gmail.com>
2025-06-16 14:47:24 -0300

    Configuração do PostgreSQL no Railway

--- ajudefacil/settings.py
+++ ajudefacil/settings.py
@@ -54,11 +54,7 @@ WSGI_APPLICATION = 'ajudefacil.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,  
-        ssl_require=True   
-    )
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
