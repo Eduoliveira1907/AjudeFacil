c390dd456b95c0dc1ad73be6f73351a2b8c1594c
Eduoliveira1907 <eduardo.l.oliveira19@gmail.com>
2025-06-16 11:50:51 -0300

    Corrige carregamento de arquivos estáticos com WhiteNoise

--- ajudefacil/settings.py
+++ ajudefacil/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -93,3 +94,5 @@ LOGIN_URL = '/login/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'doacoes/templates/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
